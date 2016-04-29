1612546e1d7d9d2cc16539dd9b18d4c147964d2a
Thomas Alrek <thomas@alrek.no>
2016-04-29 15:33:07 +0200

    Performance tweak

--- dev/Math.js
+++ dev/Math.js
@@ -7,16 +7,12 @@ Math.flip = function(value, max){
     return Math.abs(max - parseInt(value));
 }
 
-Math.toInt = function(num){
-    return num | 0;
-}
-
 Math.randomRange = function(min, max, integer){
     if(typeof min !== 'number' && typeof max !== 'number'){
         return NaN;
     }
     if(integer === true){
-        return Math.toInt(Math.random() * (max - min) + min);        
+        return Math.floor(Math.random() * (max - min) + min);        
     }
     return Math.random() * (max - min) + min;
 }
